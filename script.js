ae7f7db6e34f6dfff44aa715ad595df0d50318a8
CuteToxic <telloerick30@gmail.com>
2026-08-07 19:14:58 -0500

    Change event listener from envelope to cardContainer

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const envelope = document.getElementById("envelope");
+    const cardContainer = document.getElementById("cardContainer");
 
-    envelope.addEventListener("click", () => {
-        // Esto añade o quita la clase 'open', activando las físicas del CSS
-        envelope.classList.toggle("open");
+    cardContainer.addEventListener("click", () => {
+        cardContainer.classList.toggle("open");
     });
 });
